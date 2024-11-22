370d77b12ec912534d6b051ea0d4224907522ac3
Knubsen <ole.liebelt@gmail.com>
2024-11-22 11:40:37 +0100

    Cleared first star of first day

--- days/d1/main.go
+++ days/d1/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"unicode"
 
 	"github.com/Knubsen/aoc/utils"
 )
@@ -10,5 +11,30 @@ func main() {
 	lines, err := utils.ReadLines("")
 	utils.Check(err)
 
-	fmt.Println(lines)
+	var acc int
+	for _, line := range lines {
+		r1, r2 := getOuterDigits(line)
+		acc += combineRuneDigits(r1, r2)
+	}
+
+	fmt.Println(acc)
+}
+
+func combineRuneDigits(r1 rune, r2 rune) int {
+	return int(r1-'0')*10 + int(r2-'0')
+}
+
+func getOuterDigits(line string) (rune, rune) {
+	var first, last rune
+
+	for _, r := range line {
+		if unicode.IsDigit(r) {
+			if first == 0 {
+				first = r
+			}
+			last = r
+		}
+	}
+
+	return first, last
 }
