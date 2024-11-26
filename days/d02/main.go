ebeda8b0b5609b4565cef946099051eaad2c8924
Ole Liebelt <ole.liebelt@koerber.com>
2024-11-26 16:12:29 +0100

    Cleared first challenge of d2

--- days/d02/main.go
+++ days/d02/main.go
@@ -1,5 +1,56 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strconv"
+	"strings"
 
+	"github.com/Knubsen/aoc/utils"
+)
+
+var contained = map[string]int{
+	"red":   12,
+	"green": 13,
+	"blue":  14,
+}
+
+func main() {
+	lines, err := utils.ReadLines("")
+	utils.Check(err)
+
+	acc := 0
+	for _, line := range lines {
+		if id, possible := gameIsPossible(line, contained); possible {
+			acc += id
+		}
+	}
+
+	fmt.Println(acc)
+}
+
+func gameIsPossible(game string, content map[string]int) (int, bool) {
+	prefStructure := strings.Split(game, ":")
+	id := strings.Split(prefStructure[0], " ")[1]
+	games := strings.Split(prefStructure[1], ";")
+
+	possible := true
+
+	for _, game := range games {
+		for _, round := range strings.Split(strings.TrimSpace(game), ",") {
+			roundList := strings.Split(strings.TrimSpace(round), " ")
+
+			v := content[roundList[1]]
+			cubeCount, err := strconv.Atoi(roundList[0])
+			utils.Check(err)
+
+			if cubeCount > v {
+				possible = false
+			}
+		}
+	}
+
+	intId, err := strconv.Atoi(id)
+	utils.Check(err)
+
+	return intId, possible
 }
