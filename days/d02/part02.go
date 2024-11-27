8800eba659d4459f84a7464b4dd4bac089cf7233
Ole Liebelt <ole.liebelt@koerber.com>
2024-11-27 15:26:14 +0100

    begun work on p2 of d2

--- days/d02/part02.go
+++ days/d02/part02.go
@@ -6,11 +6,11 @@ import (
 	"github.com/Knubsen/aoc/utils"
 )
 
-// var contained = map[string]int{
-// 	"red":   12,
-// 	"green": 13,
-// 	"blue":  14,
-// }
+type game struct {
+	red   int
+	green int
+	blue  int
+}
 
 func ExecPt02() {
 	lines, err := utils.ReadLines("d02")
@@ -18,37 +18,15 @@ func ExecPt02() {
 
 	acc := 0
 	for _, line := range lines {
-		if id, possible := gameIsPossible(line, contained); possible {
-			acc += id
-		}
+		g := smallestGame(line)
+		acc += g.red * g.green * g.blue
 	}
 
 	fmt.Println(acc)
 }
 
-// func gameIsPossible(game string, content map[string]int) (int, bool) {
-// 	prefStructure := strings.Split(game, ":")
-// 	id := strings.Split(prefStructure[0], " ")[1]
-// 	games := strings.Split(prefStructure[1], ";")
-//
-// 	possible := true
-//
-// 	for _, game := range games {
-// 		for _, round := range strings.Split(strings.TrimSpace(game), ",") {
-// 			roundList := strings.Split(strings.TrimSpace(round), " ")
-//
-// 			v := content[roundList[1]]
-// 			cubeCount, err := strconv.Atoi(roundList[0])
-// 			utils.Check(err)
-//
-// 			if cubeCount > v {
-// 				possible = false
-// 			}
-// 		}
-// 	}
-//
-// 	intId, err := strconv.Atoi(id)
-// 	utils.Check(err)
-//
-// 	return intId, possible
-// }
+func smallestGame(line string) *game {
+	var currentGame game
+
+	return nil
+}
